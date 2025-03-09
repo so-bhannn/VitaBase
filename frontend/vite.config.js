7aac0eb2e9da490a8eb3aee1498a84a445034364
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-03-09 23:09:01 +0530

    tailwindcss config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  }
 })
