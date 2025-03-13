4ccdbd50bb050e48cd21d42a96c131f022c6106a
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-03-13 13:20:32 +0530

    create serializer for user

---
+++ accounts/serializers.py
@@ -0,0 +1,31 @@
+from .models import CustomUser
+from rest_framework import serializers
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model= CustomUser
+        fields=['email', 'password', 'name', 'age', 'gender', 'is_doctor', 'is_patient']
+        extra_kwargs={
+            'password':{'write_only':True},
+            'unique_id': {'read_only': True}
+        }
+
+    def validate(self, attrs):
+        is_doctor=attrs.get('is_doctor')
+        is_patient=attrs.get('is_patient')
+
+        if is_doctor and is_patient:
+            raise serializers.ValidationError("User can have only one role- Doctor or Patient.")
+        if not is_doctor and not is_patient:
+            raise serializers.ValidationError("User has no roles.")
+        return attrs
+
+    def validate_email(self, email):
+        if CustomUser.objects.filter(email=email).exists():
+            raise serializers.ValidationError("email already exists")
+        return email
+    
+    def create(self, validated_data):
+        user= CustomUser.objects.create_user(**validated_data)
+        return user
