15e5b084142dbc0413bb663d2dd199702d9be221
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-03-15 23:41:29 +0530

    add accounts in settings.py/installed_apps

--- vitabase/settings.py
+++ vitabase/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -98,6 +99,9 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+#Auth
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 
 # Internationalization
